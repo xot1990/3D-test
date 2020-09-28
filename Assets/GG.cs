5fa0fddf58b01497705169d8faded544ff889d2f
Анунах великий <guk.012v@gmail.com>
2020-09-28 08:42:07 +0300

    Start

---
+++ Assets/GG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GG : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+        if (Input.GetKey.GetAxis("Horizontal"))
+        {
+
+        }
+
+    }
+}
